5b8e650992e1c88b3cdc2ff7d7c8dd11a41d746c
Maciej Poleski <Maciej-Poleski@users.noreply.github.com>
2014-10-28 15:31:03 +0100

    A

--- satori/A/test1.cpp
+++ satori/A/test1.cpp
@@ -1,11 +1,30 @@
 #include <functional>
 #include <vector>
 #include <iostream>
+#include <random>
+
+constexpr std::size_t maxN=100000;
+constexpr std::size_t maxM=1000000;
+
+void generateEmptyGraph(std::size_t size)
+{
+    std::cout<<size<<" 0\n";
+}
 
 std::vector<std::function<void()>> prepareTests()
 {
+    std::mt19937 engine(404);
+    std::uniform_int_distribution<std::size_t> vertexRangeDistribution(1,maxN);
+    auto vertexNumerGenerator = std::bind(vertexRangeDistribution,engine);
+    
     std::vector<std::function<void()>> tests=
     {
+        std::bind(generateEmptyGraph,1),
+        std::bind(generateEmptyGraph,2),
+        std::bind(generateEmptyGraph,maxN),
+        std::bind(generateEmptyGraph,vertexNumerGenerator()),
+        std::bind(generateEmptyGraph,vertexNumerGenerator()),
+        std::bind(generateEmptyGraph,vertexNumerGenerator()),
         
     };
     
