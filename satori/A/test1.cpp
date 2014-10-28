746acb9e1f4f3ae80b46b34e157e260b6374898b
Maciej Poleski <Maciej-Poleski@users.noreply.github.com>
2014-10-28 22:36:17 +0100

    A

--- satori/A/test1.cpp
+++ satori/A/test1.cpp
@@ -11,12 +11,22 @@ void generateEmptyGraph(std::size_t size)
     std::cout<<size<<" 0\n";
 }
 
-std::vector<std::function<void()>> prepareTests()
+template<class Generator>
+void generateRandomEdges(Generator& engine, const std::size_t vertices, const std::size_t edges)
 {
-    std::mt19937 engine(404);
+    std::uniform_int_distribution<int> vertexGenerator(1,vertices);
+    auto vertex=std::bind(vertexGenerator,std::ref(engine));
+    std::cout<<vertices<<' '<<edges<<'\n';
+    for(std::size_t i=0;i<edges;++i)
+	std::cout<<vertex()<<' '<<vertex()<<'\n';
+}
+
+template<class Generator>
+std::vector<std::function<void()>> prepareTests(Generator& engine)
+{
     std::uniform_int_distribution<std::size_t> vertexRangeDistribution(1,maxN);
-    auto vertexNumerGenerator = std::bind(vertexRangeDistribution,engine);
-    
+    auto vertexNumerGenerator = std::bind(vertexRangeDistribution,std::ref(engine));
+
     std::vector<std::function<void()>> tests=
     {
         std::bind(generateEmptyGraph,1),
@@ -24,10 +34,19 @@ std::vector<std::function<void()>> prepareTests()
         std::bind(generateEmptyGraph,maxN),
         std::bind(generateEmptyGraph,vertexNumerGenerator()),
         std::bind(generateEmptyGraph,vertexNumerGenerator()),
-        std::bind(generateEmptyGraph,vertexNumerGenerator()),
-        
+        std::bind(generateEmptyGraph,vertexNumerGenerator())
     };
-    
+
+    for(const std::size_t vc : {5ul,10ul,20ul,50ul,maxN})	// Liczba wierzchołków
+	for(const std::size_t ec :{vc, vc*2, vc*vc, vc*vc*2})	// Liczba krawędzi
+	{
+	    if(ec>maxM)
+		continue;
+	    for(std::size_t i=0;i<3;++i)			// Zestawy
+		tests.push_back([&engine,vc,ec]{ generateRandomEdges(engine,vc,ec); });
+	}
+
+
     return tests;
 }
 
@@ -41,5 +60,6 @@ void runTests(const std::vector<std::function<void()>>& tests)
 int main()
 {
     std::ios_base::sync_with_stdio(false);
-    runTests(prepareTests());
+    std::mt19937 engine(404);
+    runTests(prepareTests(engine));
 }
